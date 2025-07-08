9483c614ef6f611301742634b09368ce3faf75f8
samir5243d <samir5243d@gmail.com>
2025-07-08 21:52:36 +0530

    updated existing files

---
+++ vidio54/new.js
@@ -0,0 +1,5 @@
+console.log("HELLO WORLD")
+console.log("CODE IS RUNNING")
+console.log("CODE IS ALSO RUNNING")
+
+console.log("CODE IS LOOKING LIKE A WOW")
---
+++ vidio55/script.js
@@ -0,0 +1,19 @@
+console.log("Hey this is tutoriol 55 of js ")
+
+let a = 5;
+// a=a+1;
+let b = 6;
+let c = "Harry";
+let _a = 34; // This is a valid variable name, but not recommended to use reserved words like 'var' as variable names.
+// 55var = 45; // This is not a valid variable name, as it starts with a number.
+
+// console.log(a + b + 8);
+// console.log(typeof a, typeof b, typeof c);
+{
+    let a = 45; // This 'a' is a different variable scoped to this block.
+    console.log(a)
+}
+console.log(a); // This will log the outer 'a' which is 5.
+
+const a1=6; 
+// a1=a1+1; // This will throw an error because 'a1' is a constant and cannot be reassigned.
