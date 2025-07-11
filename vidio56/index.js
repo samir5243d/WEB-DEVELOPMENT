2c4890e55410f3c3995aff43ccd5b714d34eea1b
samir5243d <samir5243d@gmail.com>
2025-07-11 16:39:44 +0530

    added new files

---
+++ vidio56/index.js
@@ -0,0 +1,21 @@
+console.log("Hello I am  conditional tutorial");
+
+let age =3;
+let grace=2;
+
+
+
+console.log(age + grace)
+console.log(age - grace)
+console.log(age * grace)
+console.log(age / grace)
+console.log(age ** grace)
+console.log(age % grace)
+
+if((age+grace) > 18){
+    console.log("You can drive");
+}
+
+else{
+    console.log("You cannot drive");
+}
---
+++ vidio56/index2.js
@@ -0,0 +1,25 @@
+let age = 18;
+
+if(age != 18){
+    console.log("YOU CAN DRIVE");
+}
+
+else if(age==0){
+    console.log("ARE YOU KIDDING");
+}
+
+else{
+    console.log("YOU CANNOT DRIVE");
+}
+
+a=6;
+b=8;
+let c = a > b ? (a-b) : (b-a);// Ternary operator
+
+/*this ternary  operator translates to  
+if(a>b){
+   let c=a-b;
+}
+   else{
+    let c=a-b;
+}*/
