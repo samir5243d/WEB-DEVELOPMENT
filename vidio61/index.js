2704806e451962b77c7a937822d9d088b3041228
samir5243d <samir5243d@gmail.com>
2025-07-18 21:13:55 +0530

    added new shit

---
+++ vidio61/index.js
@@ -0,0 +1,20 @@
+// CREATING FAULTY CALCULATOR USING JAVASCRIPT
+
+let random = Math.random()
+
+let a=prompt("Enter first number:");
+let b=prompt("Enter second  number:");
+let c=prompt("Enter operation:"); = {
+
+}
+
+let obj={"+": "-",}
+
+if(random>0.1){
+    // perform correct calculation
+
+
+    }
+    else{
+    // perform faulty calculation
+    }
