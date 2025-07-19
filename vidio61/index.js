44ff09b4e776f34ff3000989577725b04fc1454f
samir5243d <samir5243d@gmail.com>
2025-07-19 21:47:52 +0530

    added new files

--- vidio61/index.js
+++ vidio61/index.js
@@ -1,20 +1,23 @@
 // CREATING FAULTY CALCULATOR USING JAVASCRIPT
 
-let random = Math.random()
+let random = Math.random();
 
-let a=prompt("Enter first number:");
-let b=prompt("Enter second  number:");
-let c=prompt("Enter operation:"); = {
+let a = prompt("Enter first number:");
+let b = prompt("Enter second  number:");
+let c = prompt("Enter operation:");
 
+let obj = { "+": "-", "*": "+", "-": "/", "/": "*" };
+
+if (random > 0.1) {
+    // Perform correct calculation
+    console.log(`The result is ${a} ${c} ${b}`)
+    alert(`The result is ${eval(`${a} ${c} ${b}`)}`)
+    
 }
 
-let obj={"+": "-",}
+else {
+    // Perform wrong calculation
+    c = obj[c]
+    alert(`The result is ${eval(`${a} ${c} ${b}`)}`) 
 
-if(random>0.1){
-    // perform correct calculation
-
-
-    }
-    else{
-    // perform faulty calculation
-    }
+}
