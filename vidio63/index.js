1b92f31822f148c7b6386ee0b91384d130be6a0d
samir5243d <samir5243d@gmail.com>
2025-07-27 22:48:10 +0530

    started learning array

---
+++ vidio63/index.js
@@ -0,0 +1,17 @@
+let a = [1, 2, 3, 4, 5];
+// Index 0,1,2,3,4
+
+
+arr[0]=5666;
+
+console.log(a);
+
+console.log(a.length);
+
+console.log(a[0]);
+
+console.log(a[1]);
+
+console.log(a[2]);
+
+console.log(a[3]);
