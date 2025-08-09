49dabdac02b805c3d5623e88822ccca17b95cd7a
samir5243d <samir5243d@gmail.com>
2025-08-09 23:48:35 +0530

    added stuff

--- vidio63/index.js
+++ vidio63/index.js
@@ -1,21 +1,32 @@
-let arr = [1, 2, 3, 4, 5];
-// Index 0,1,2,3,4
+// let arr = [1, 2, 3, 4, 5];
+// // Index 0,1,2,3,4
 
 
-arr[0]=5666;
+// arr[0]=5666;
 
-// console.log(arr, typeof arr);
+// // console.log(arr, typeof arr);
  
-// console.log(arr.length);
+// // console.log(arr.length);
 
-// console.log(arr[0])
+// // console.log(arr[0])
 
-// console.log(arr[1])
+// // console.log(arr[1])
 
-// console.log(arr[2])
+// // console.log(arr[2])
 
-// console.log(arr[3])
+// // console.log(arr[3])
 
 
-console.log(arr.toString())// "5666,2,3,4,5"
-console.log(arr.join(" and "))
+// console.log(arr.toString())// "5666,2,3,4,5"
+// console.log(arr.join(" and "))
+
+// let a1 = [1,2,3]
+// let a2 = [4,5,6]
+// let a3 = [9,8,7]
+
+// console.log(a1.concat(a2,a3))
+
+
+
+let numbers=[1,2,3,4,5]
+console.log(numbers.splice(1,3))
