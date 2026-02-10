d415c76967d78595db81d72eff1985d029f73ae5
samir5243d <samir5243d@gmail.com>
2026-02-10 11:56:49 +0530

    learned how to generate random no using if else

--- vidio63/loops.js
+++ vidio63/loops.js
@@ -10,3 +10,17 @@ let a = [1, 93, 5, 6, 88]
 a.forEach((value, index, arr)=>{
     console.log(value, index , arr)
 })
+
+
+let obj= {
+
+    a:1,
+    b:2,
+    c:3
+}
+for (const key in obj)  {
+    if( Object.hasOwnProperty.call(obj, key) ) {
+        const element = obj[key];
+        console.log( key, element)
+    }
+}
---
+++ vidio63/mfr.js
@@ -0,0 +1,8 @@
+// let arr = [1, 13, 5, 7, 11];
+
+// let newarr = []
+
+// for (let index = 0; index< arr.length; index++){
+//     const element = arr[index];
+//     newArr.push(element**2)
+// }
