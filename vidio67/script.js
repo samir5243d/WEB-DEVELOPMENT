f56f6604540479b5f6676337d46b173bdfba8806
samir5243d <samir5243d@gmail.com>
2026-02-13 11:03:27 +0530

    added new stuff

---
+++ vidio67/script.js
@@ -0,0 +1,5 @@
+console.log("hello world")
+
+document.body.firstElementChild // first element of body tag
+document.body.firstElementChild.childNodes// text nodes and  comment nodes
+document.body.firstElementChild.children// only element nodes
